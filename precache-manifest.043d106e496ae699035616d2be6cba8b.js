5f580b3ee7d2c14f997e8f5ad294f77dab989106
AnhLinhVTCA <linhna.nde18090@vtc.edu.vn>
2020-06-19 14:07:14 +0700

    finish

---
+++ precache-manifest.043d106e496ae699035616d2be6cba8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd0fbfc6fb5ecb1ce0940c09320c560",
+    "url": "./index.html"
+  },
+  {
+    "revision": "abae3427199b6809b1d8",
+    "url": "./static/css/main.4506476f.chunk.css"
+  },
+  {
+    "revision": "64c85887de55f3f85dad",
+    "url": "./static/js/2.114606a3.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "./static/js/2.114606a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abae3427199b6809b1d8",
+    "url": "./static/js/main.4417dd52.chunk.js"
+  },
+  {
+    "revision": "38b2ee104b6278312ba6",
+    "url": "./static/js/runtime-main.8964b653.js"
+  }
+]);
